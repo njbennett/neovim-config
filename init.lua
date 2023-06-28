93074d758b29afe1142c7e693d83eca9bdab089d
Nat Bennett <natalie.j.bennett@gmail.com>
2023-06-27 18:35:12 -0700

    Add plugin manager & neosolarized color scheme

---
+++ init.lua
@@ -0,0 +1,32 @@
+--- bootstrap plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+--- load plugins
+plugins = {
+  { 'overcache/NeoSolarized' },
+}
+require("lazy").setup(plugins)
+
+--- set line numbers for pairing
+vim.opt.number = true
+
+--- enable syntax highlighting
+vim.opt.syntax = "on"
+
+--- enable 24-bit RGB colors
+vim.opt.termguicolors = true
+
+--- set the colorscheme
+vim.cmd("colorscheme NeoSolarized")
